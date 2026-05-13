9cec18e6a82663a856b6eb058662deda69dc34e6
Eya Guizani <guizanieya710@gmail.com>
2026-05-13 22:11:27 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/MiniProjetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetSpringBootApplication.class, args);
+	}
+
+}
